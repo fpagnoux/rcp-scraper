c39706fadeb5121495c7e2d624b984fb1f727cc3
fpagnoux <fpagnoux@octo.com>
2015-08-23 15:23:02 +0200

    Implement TableItem to preserve order of fields

--- bernie/items.py
+++ bernie/items.py
@@ -5,13 +5,60 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Field
+from scrapy.item import BaseItem
+from collections import MutableMapping, OrderedDict
 
+class TableItem(MutableMapping, BaseItem):
 
-class BernieItem(scrapy.Item):
-	
-	#Dynamically creates field
-	def __setitem__(self, key, value):
-		if key not in self.fields:
-			self.fields[key] = scrapy.Field()
-		self._values[key] = value
+	# Reimplementation of DictItem (https://github.com/scrapy/scrapy/blob/master/scrapy/item.py#L45)
+	# Allows dynamical field creation
+	# Preserves the order of the fields
+	# Convenient for table
+
+    fields = OrderedDict()
+
+    def __init__(self, *args, **kwargs):
+        self._values = OrderedDict()
+        if args or kwargs:  # avoid creating dict for most common case
+            for k, v in six.iteritems(OrderedDict(*args, **kwargs)):
+                self[k] = v
+
+    def __getitem__(self, key):
+        return self._values[key]
+
+    def __setitem__(self, key, value):
+        if key not in self.fields:
+            self.fields[key] = Field()
+        self._values[key] = value
+
+    def __delitem__(self, key):
+        del self._values[key]
+
+    def __getattr__(self, name):
+        if name in self.fields:
+            raise AttributeError("Use item[%r] to get field value" % name)
+        raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        if not name.startswith('_'):
+            raise AttributeError("Use item[%r] = %r to set field value" %
+                (name, value))
+        super(TableItem, self).__setattr__(name, value)
+
+    def __len__(self):
+        return len(self._values)
+
+    def __iter__(self):
+        return iter(self._values)
+
+    __hash__ = BaseItem.__hash__
+
+    def keys(self):
+        return self._values.keys()
+
+    def __repr__(self):
+        return pformat(dict(self))
+
+    def copy(self):
+        return self.__class__(self)
--- bernie/spiders/spider.py
+++ bernie/spiders/spider.py
@@ -1,28 +1,28 @@
 import scrapy
-from bernie.items import BernieItem
+from bernie.items import TableItem
 
 class spider(scrapy.Spider):
-	name = "bernieSpider"
-	start_urls = ["file:///home/fpagnoux/erika/national.html"]
+    name = "bernieSpider"
+    start_urls = ["file:///home/fpagnoux/erika/national.html"]
 
-	def parse(self, response):
-		table = response.css('.data').pop()
-		legend = table.css('tr')[0]
-		fieldNames = legend.css('th::text').extract()
-		nb_fields = len(fieldNames)
-		items = []
+    def parse(self, response):
+        table = response.css('.data').pop()
+        legend = table.css('tr')[0]
+        fieldNames = legend.css('th::text').extract()
+        nb_fields = len(fieldNames)
+        items = []
 
-		contentLines = table.css('tr')[1::]
+        contentLines = table.css('tr')[1::]
 
-		for line in contentLines:
-			item = BernieItem()
-			values = line.css('td::text, td span::text, td a::text').extract()
-			for i in range(nb_fields):
-				item[fieldNames[i]] = values[i]
+        for line in contentLines:
+            item = TableItem()
+            values = line.css('td::text, td span::text, td a::text').extract()
+            for i in range(nb_fields):
+                item[fieldNames[i]] = values[i]
 
-			items.append(item)
+            items.append(item)
 
-		return items
+        return items
 
 
 
