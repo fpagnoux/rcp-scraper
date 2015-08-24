64d12eb12d67d60a31654aaff59fc22429e3a6f3
fpagnoux <fpagnoux@octo.com>
2015-08-24 11:01:03 +0200

    tab => spaces

--- scraper.py
+++ scraper.py
@@ -2,19 +2,19 @@ import sys, os
 args = sys.argv
 
 if (len(args) <= 1):
-	print("ERROR: Please provide source url")
-	print("Example : python scraper.py url output.csv")
+    print("ERROR: Please provide source url")
+    print("Example : python scraper.py url output.csv")
 else:
-	url = args[1]
-	if (len(args) == 2):
-		filename = url.split('/')[-1].split('.')[0]
-		output = filename + ".csv"
-		print("No output file specified : using " + output)
-	else:
-		output = sys.argv[2]
-		if not output.endswith(".csv"):
-			output = output + ".csv"
-	if os.path.isfile(output):
-		os.remove(output)
-	os.system("scrapy crawl realclearpoliticsSpider -a url="+url+" -o "+output)
+    url = args[1]
+    if (len(args) == 2):
+        filename = url.split('/')[-1].split('.')[0]
+        output = filename + ".csv"
+        print("No output file specified : using " + output)
+    else:
+        output = sys.argv[2]
+        if not output.endswith(".csv"):
+            output = output + ".csv"
+    if os.path.isfile(output):
+        os.remove(output)
+    os.system("scrapy crawl realclearpoliticsSpider -a url="+url+" -o "+output)
 
