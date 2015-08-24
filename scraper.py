892167e9d3d68715b9ce1879900dfd009f82c650
fpagnoux <fpagnoux@octo.com>
2015-08-24 11:00:09 +0200

    Force output to be csv

--- scraper.py
+++ scraper.py
@@ -12,6 +12,8 @@ else:
 		print("No output file specified : using " + output)
 	else:
 		output = sys.argv[2]
+		if not output.endswith(".csv"):
+			output = output + ".csv"
 
 	os.system("scrapy crawl realclearpoliticsSpider -a url="+url+" -o "+output)
 
